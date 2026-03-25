c2331864b1aeda0520fed2ed041435c67b4fd3c0
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-25 10:35:59 -0500

    Change GradeLetter from 'A' to 'a'

--- VariableTypes.cs
+++ VariableTypes.cs
@@ -2,7 +2,7 @@
 public class VariableTypes
 {   
  public string AnimalName = "cupcake";
- public char GradeLetter = "A";
+ public char GradeLetter = "a";
  public int Year = 2;
  public double Height = 6.4;
  public bool IsIndoorPet = true;
