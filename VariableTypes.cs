81e8688b6b3954e8b630ed8eb9a4b241e28ebcc9
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-25 10:40:16 -0500

    Fix formatting and encoding issues in VariableTypes.cs

--- VariableTypes.cs
+++ VariableTypes.cs
@@ -2,7 +2,7 @@
 public class VariableTypes
 {   
  public string AnimalName = "cupcake";
- public char GradeLetter = "a";
+ public char GradeLetter = 'a';
  public int Year = 2;
  public double Height = 6.4;
  public bool IsIndoorPet = true;
