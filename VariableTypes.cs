6c6b1fd76ae1603149d0178c818e466585b78b9f
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-25 10:34:50 -0500

    Define class-level variables in VariableTypes

--- VariableTypes.cs
+++ VariableTypes.cs
@@ -1,10 +1,9 @@
 ﻿namespace knightmoves;
 public class VariableTypes
 {   
- public 
+ public string AnimalName = "";
+ public char GradeLetter = "A";
+ public int Year = 2;
+ public double Height = 6.4;
+ public bool IsIndoorPet = true;
 }
-should define a public class-level string variable named AnimalName
-should define a public class-level character variable named GradeLetter (like: A, B, C, D, or F)
-should define a public class-level numeric whole number variable named Year
-should define a public class-level numeric decimal number variable named Height (like: 5.5, 6.2, etc)
-should define a public class-level boolean variable named IsIndoorPet
