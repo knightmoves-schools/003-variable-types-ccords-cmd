d026321d7a1d014ca4546ca2e729d60cbefc808b
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-25 10:27:47 -0500

    Add class-level variables to VariableTypes
    Define class-level variables for AnimalName, GradeLetter, Year, Height, and IsIndoorPet.

--- VariableTypes.cs
+++ VariableTypes.cs
@@ -1,5 +1,10 @@
 ﻿namespace knightmoves;
 public class VariableTypes
 {   
-    // Add your code here
+ public 
 }
+should define a public class-level string variable named AnimalName
+should define a public class-level character variable named GradeLetter (like: A, B, C, D, or F)
+should define a public class-level numeric whole number variable named Year
+should define a public class-level numeric decimal number variable named Height (like: 5.5, 6.2, etc)
+should define a public class-level boolean variable named IsIndoorPet
